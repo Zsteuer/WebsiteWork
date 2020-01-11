9b78ec290d222289df6de0e9eb78d38fa5f7862e
Zach Steuer <zsteuer@oberlin.edu>
2020-01-11 17:29:10 -0500

    got it functional. Just need to take care of warnings

--- src/ButtonsAndVideoFrameThatChanges.jsx
+++ src/ButtonsAndVideoFrameThatChanges.jsx
@@ -15,7 +15,8 @@ import { Menu, MenuItem} from '@material-ui/core';
 import {SocialIcon} from "react-social-icons";
 
 const MenuOptions = { // I realized I'd have to make a row of buttons for desktop and a dropdown for mobile
-    BANDCAMP: {title: "bandcamp",
+    BANDCAMP: {
+        title: "bandcamp",
         desktopUrl: "http://zakbullet.bandcamp.com",
         mobileUrl: "http://zakbullet.bandcamp.com"},
     WORDPRESS: {
@@ -26,7 +27,7 @@ const MenuOptions = { // I realized I'd have to make a row of buttons for deskto
     YOUTUBESOLOGUITAR:{
         title: "solo guitar",
         desktopUrl: "http://www.youtube.com/embed/47QNJBAYXDw",
-        mobileUrl: "https://www.youtube.com/watch?v=47QNJBAYXDw",
+        mobileUrl: "http://www.youtube.com/embed/47QNJBAYXDw",
     },
     VIMEOSOUNDTRACK:{
         title: "Soundtrack",
@@ -36,26 +37,28 @@ const MenuOptions = { // I realized I'd have to make a row of buttons for deskto
     YOUTUBESOUNDDESIGN: {
         title: "Sound Design Pedagogy",
         desktopUrl: "http://www.youtube.com/embed/P6pmyqCv8sQ",
-        mobileUrl: "https://www.youtube.com/watch?v=P6pmyqCv8sQ",
+        mobileUrl: "http://www.youtube.com/embed/P6pmyqCv8sQ",
     },
     YOUTUBELIVESHOW: {
         title: "DJing with live guitar loops",
         desktopUrl: "http://www.youtube.com/embed/r4QoZywqcL4",
-        mobileUrl: "https://www.youtube.com/watch?v=r4QoZywqcL4"
+        mobileUrl: "http://www.youtube.com/embed/r4QoZywqcL4"
     }
 }
+
+const defaultMobileMenuText = "Choose one";
 
 class ButtonsAndVideoFrameThatChanges extends Component{
 
 constructor(props){
         super(props);
         this.state = {
-            shouldBeHidden: true,
-            urlToDisplayInFrame: "http://www.wikipedia.org",
+            shouldBeHidden: true, // this controls the iframe for both the mobile and desktop frames
+            urlToDisplayInFrame: null,
             showAboutMeModal: false,
             showContactModal: false,
             mobileMenuOpen: false,
-            showMobileEmbeddedSite: false};
+            mobileMenuText: defaultMobileMenuText};
     }
 
     openAboutMeModel = () =>{
@@ -90,7 +93,26 @@ constructor(props){
         this.setState({mobileMenuOpen: false});
     }
 
+    handleMobileMenuOptions = (option) =>{
+        this.setState(
+            {
+                mobileMenuOpen : false,
+                shouldBeHidden : false,
+            mobileMenuText : option.title,
+            urlToDisplayInFrame: option.mobileUrl,
+           });
+    }
+
+    resetMobileOptions = () =>{
+        this.setState({
+            shouldBeHidden: true,
+            mobileMenuText: defaultMobileMenuText,
+            mobileMenuOpen: false});
+    }
+
     render(){
+        let windowHeight = window.innerHeight;
+        let windowWidth = window.innerWidth;
         const desktopButtons = [];
         let desktopButtonNum = 0;
         for (const e of Object.keys(MenuOptions)){
@@ -99,13 +121,19 @@ constructor(props){
         </div>)
             desktopButtonNum++;
         }
+        const mobileMenuOptions = [];
+        for (const e of Object.keys(MenuOptions)){
+            mobileMenuOptions.push(<MenuItem onClick={() => this.handleMobileMenuOptions(MenuOptions[e])}>
+                {MenuOptions[e].title}
+            </MenuItem>)
+        }
         return(
             <div>
                 <BrowserView>
                  <SocialMediaToolbar className='floatontop'/>
                 </BrowserView>
                 <MobileView>
-                    <div className="header" style={{ backgroundColor: '#90ee90' }}>
+                    <div className="header" style={{ backgroundColor: '#90ee90', zIndex: 9999 }}>
                                 <div className="makerows" style={{ padding: '6px' }}>
                                     {/* "header" has fixed 48px */}
                                     <div/>
@@ -180,6 +208,7 @@ constructor(props){
                         <h3>Welcome to my music site. Click the buttons {isMobile ? "below" : "above"} to get started! If you were looking for a software engineering portfolio (including the source code for this site) check out: <a href="https://github.com/Zsteuer">https://github.com/Zsteuer</a></h3>
                 </div>
                 <div className="secondInTwo">
+                    <BrowserView>
                     <div>
                         <div>
                             {this.state.shouldBeHidden? null: <ButtonWithIframe text="Close embedded page" variant="danger" onClickFunc={() => this.hideEmbeddedPage() }/> }
@@ -187,6 +216,7 @@ constructor(props){
                         {this.state.shouldBeHidden? null : <Iframe url={this.state.urlToDisplayInFrame}
                         width="1080px" height="720px" display="initial" position="relative"/> }
                     </div>
+                    </BrowserView>
                 </div>
                 <div className = {isMobile? "secondInTwoMobile" : "secondInTwo"} >
                     <div className = 'makerows' position="relative">
@@ -198,18 +228,24 @@ constructor(props){
                     </div>
                 </div>
                 <MobileView>
-                    <Button className="secondInTwo" variant="dark" size="lg" style={{ marginTop : "25px"}} onClick={() => this.openMobileMenu()}>Choose one
+                    <Button id="openMobileMenuButton" className="secondInTwo" variant="dark" size="lg" style={{ marginTop : "25px", marginBottom: "0px"}} onClick={() => this.openMobileMenu()}>{this.state.mobileMenuText}
                     </Button>
-                    <div className="secondInTwo">
+                    <div className="secondInTwo" style={{ marginTop : "0px"}}>
+                        <div>
                         <Menu
                             id="simple-menu"
                             open={this.state.mobileMenuOpen}
                             onClose={() => this.closeMobileMenu()}
                         >
-                            <MenuItem>Profile</MenuItem>
-                            <MenuItem>My account</MenuItem>
-                            <MenuItem>Logout</MenuItem>
+                            {mobileMenuOptions}
+                            <MenuItem onClick={() => this.resetMobileOptions()}>Close</MenuItem>
                         </Menu>
+                        {this.state.shouldBeHidden? null : <Iframe style={{marginTop: "0px"}} url={this.state.urlToDisplayInFrame}
+                                                                   width={windowWidth*.9 + "px"} height={windowHeight*.9 + "px"} display="initial" position="relative"/>
+                        }
+                        </div>
+                    </div>
+                    <div className="secondInTwo" style={{height:"100%", width: "100%", marginTop: "0px"}}>
                     </div>
                 </MobileView>
             </div>
