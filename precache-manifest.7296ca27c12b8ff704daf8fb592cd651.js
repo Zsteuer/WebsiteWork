f58d405ef42ce3c3bff706e75fc3047d377db964
Zach Steuer <zsteuer@oberlin.edu>
2020-01-24 20:11:20 -0500

    Updates

---
+++ precache-manifest.7296ca27c12b8ff704daf8fb592cd651.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "040648c5460645f640530e6d864b3678",
+    "url": "/WebsiteWork/index.html"
+  },
+  {
+    "revision": "ec4f6fccf2924e5cafea",
+    "url": "/WebsiteWork/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "27d4782e214d1d6480dc",
+    "url": "/WebsiteWork/static/css/main.e79d629f.chunk.css"
+  },
+  {
+    "revision": "ec4f6fccf2924e5cafea",
+    "url": "/WebsiteWork/static/js/2.7911f633.chunk.js"
+  },
+  {
+    "revision": "a240044ac5a388d66a4058952bed4f18",
+    "url": "/WebsiteWork/static/js/2.7911f633.chunk.js.LICENSE"
+  },
+  {
+    "revision": "27d4782e214d1d6480dc",
+    "url": "/WebsiteWork/static/js/main.531fa329.chunk.js"
+  },
+  {
+    "revision": "0f20ca6699a9227d3fc5",
+    "url": "/WebsiteWork/static/js/runtime-main.908a1889.js"
+  },
+  {
+    "revision": "a9d94b86e19167605a41169f30344fb5",
+    "url": "/WebsiteWork/static/media/zblogosoftblue.a9d94b86.png"
+  }
+]);
